bc7775685be593759ef67d944dfe16ea64139671
KirillRV <kirillreges639@gmail.com>
2025-06-01 02:33:58 +0300

    Fixed AdminController,AuthController, GameController(fixed double /api issue)

--- casino/src/main/java/com/tms/casino/controller/GameController.java
+++ casino/src/main/java/com/tms/casino/controller/GameController.java
@@ -5,12 +5,13 @@ import com.tms.casino.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/games")
+@RequestMapping("/v1/games")
 @RequiredArgsConstructor
 public class GameController {
 
