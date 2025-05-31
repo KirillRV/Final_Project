8485aaa52b41da9a96cc67b5564f27a783b0c1eb
KirillRV <kirillreges639@gmail.com>
2025-05-31 16:21:03 +0300

    Added GameController

---
+++ casino/src/main/java/com/tms/casino/controller/GameController.java
@@ -0,0 +1,28 @@
+package com.tms.casino.controller;
+
+import com.tms.casino.model.Game;
+import com.tms.casino.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/games")
+@RequiredArgsConstructor
+public class GameController {
+
+    private final GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<List<Game>> getAllActiveGames() {
+        return ResponseEntity.ok(gameService.getAllActiveGames());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getGameDetails(@PathVariable Long id) {
+        return ResponseEntity.ok(gameService.getGameById(id));
+    }
+}
