5aacdb8c5a4be11048d5918161a80de0688fb23f
KirillRV <kirillreges639@gmail.com>
2025-06-01 23:20:56 +0300

    Updated Controllers

--- casino/src/main/java/com/tms/casino/controller/AuthController.java
+++ casino/src/main/java/com/tms/casino/controller/AuthController.java
@@ -2,34 +2,26 @@ package com.tms.casino.controller;
 
 import com.tms.casino.model.dto.AuthRequest;
 import com.tms.casino.model.dto.AuthResponse;
-import com.tms.casino.model.dto.RefreshTokenRequest;
+import com.tms.casino.model.dto.RegisterRequest;
 import com.tms.casino.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/auth")
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+    private final AuthService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest request) {
-        return ResponseEntity.ok(authService.register(request));
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        return ResponseEntity.ok(authService.login(request));
-    }
-
-    @PostMapping("/refresh")
-    public ResponseEntity<AuthResponse> refreshToken(@RequestBody RefreshTokenRequest request) {
-        return ResponseEntity.ok(authService.refreshToken(request.getRefreshToken()));
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 }
